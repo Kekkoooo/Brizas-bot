741da5e76a04504ab22e33b542834904844a02b5
Kekkoooo <93880355+Kekkoooo@users.noreply.github.com>
2022-03-15 22:59:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const ownerNumber = ["3546329122@s.whatsapp.net","14054613767@s.whatsapp.net" , "393277197374@s.whatsapp.net"]
 // mude somente o numero e não tire o @s.whatsapp.net
 
-const OriginalOwner = '3546329122' , '14054613767' , '393277197374'
+const OriginalOwner = '3546329122, 14054613767, 393277197374' 
 // mude para seu numero
 
 const callbotmsgs = ['orion', 'Kekko']
